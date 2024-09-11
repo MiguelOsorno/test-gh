3fead388c4b1daea08947ee14a7f424436451d27
miguelosorno <miguel.osornolinares@gmail.com>
2024-09-11 13:14:46 -0600

    feat(app): change desc

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Esta app esta muy cool!
         </a>
       </header>
     </div>
